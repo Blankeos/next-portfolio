b1e52ac410b726056230e8cd3477c9f87a0dcf96
Blankeos <carloantonioct@gmail.com>
2021-07-31 03:45:58 +0800

    Added Nav

---
+++ components/Nav.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from "./Container";
+
+const Nav = () => {
+  return (
+    <nav className="h-20 w-full">
+      <Container className=" flex items-center justify-between">
+        <div className="font-extrabold">CATT</div>
+        <div>
+          <p>Projects</p>
+        </div>
+      </Container>
+    </nav>
+  );
+};
+
+export default Nav;
