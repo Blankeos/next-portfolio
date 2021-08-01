3d66dbcfc1334d44fb50efd48cad701c55bd1fed
Blankeos <carloantonioct@gmail.com>
2021-08-02 02:17:47 +0800

    Polished the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Container from "./Container";
 
 const Footer = () => {
   return (
-    <footer className="bg-red-200">
+    <footer className="bg-black text-white">
       <Container>
         <div>
           <h1 className="mb-5">Carlo Taleon Portfolio WIP</h1>
@@ -12,7 +12,7 @@ const Footer = () => {
         <Link href="https://www.figma.com/proto/3mchgUMnlaD0vim3jq0eAy/Portfolio-Project?node-id=103%3A7&scaling=min-zoom">
           <a
             target="_blank"
-            className="text-6xl hover:bg-gray-800 hover:text-white transition"
+            className="text-6xl hover:bg-white hover:text-black transition"
           >
             Figma Prototype
           </a>
