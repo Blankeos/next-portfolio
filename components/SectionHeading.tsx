b001ea146ddb875e5ff58bbdf0d5bcb38035e130
Blankeos <carloantonioct@gmail.com>
2021-08-03 04:57:38 +0800

    Created a reusable Section Heading component

---
+++ components/SectionHeading.tsx
@@ -0,0 +1,19 @@
+interface SectionHeadingProps {
+  text: [string] | [string, string];
+  className?: string;
+}
+
+const SectionHeading = ({
+  text,
+  className = "font-light text-5xl",
+}: SectionHeadingProps) => {
+  return (
+    <h2 className={`flex flex-col ${className}`}>
+      {text.map((stringVal, i) => {
+        return <span className={i === 1 ? "ml-16" : ""}>{stringVal}</span>;
+      })}
+    </h2>
+  );
+};
+
+export default SectionHeading;
