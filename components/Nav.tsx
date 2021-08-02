777ddf62195bb6d4d64228a98b1ac5de11615a60
Blankeos <carloantonioct@gmail.com>
2021-08-03 04:56:29 +0800

    Updated Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,7 +4,7 @@ import Container from "./Container";
 const Nav = () => {
   return (
     <nav className="h-20 w-full">
-      <Container className=" flex items-center justify-between">
+      <Container className="flex items-center justify-between">
         <div className="font-extrabold">CATT</div>
         <div>
           <p>Projects</p>
