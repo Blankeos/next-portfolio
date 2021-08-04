8b60497b6e6ad9710dc453cf6524412e3118a0d6
Blankeos <carloantonioct@gmail.com>
2021-08-04 20:54:59 +0800

    Working SideNav intersection observer

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,20 +1,29 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import Link from "next/link";
 import Container from "./Container";
 
-const SideNav = () => {
+import { useInView } from "react-intersection-observer";
+
+interface SideNavProps {
+  isVisible?: boolean;
+}
+
+const SideNav: React.FC<SideNavProps> = ({ isVisible = true }) => {
   return (
-    <Container className="w-full sticky top-0">
-      <div className="w-20">
-        <div className="flex flex-col items-end space-y-4 text-sm font-light py-16">
-          <span>projects</span>
-          <span>skills</span>
-          <span>about me</span>
-          <span>contact</span>
-        </div>
-      </div>
-    </Container>
+    <>
+      {isVisible && (
+        <Container className="flex items-start w-full sticky top-0 z-10">
+          <div>
+            <div className="flex flex-col items-end space-y-4 text-sm font-light py-16 bg-red-500d">
+              <span>projects</span>
+              <span>skills</span>
+              <span>about me</span>
+              <span>contact</span>
+            </div>
+          </div>
+        </Container>
+      )}
+    </>
   );
 };
 
