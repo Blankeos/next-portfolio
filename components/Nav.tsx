dbe4947be87b59ad859d60bbdf35e6ca555f9aa2
Blankeos <carloantonioct@gmail.com>
2021-08-05 22:12:35 +0800

    Nav Link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Container from "./Container";
+import Link from "next/link";
 
 const Nav = () => {
   return (
     <nav className="h-20 w-full">
       <Container className="flex items-center justify-between">
-        <div className="font-extrabold select-none cursor-pointer h-full flex items-center px-5">
-          CATT
-        </div>
+        <Link href="/">
+          <a className="font-extrabold select-none cursor-pointer h-full flex items-center px-5">
+            CATT
+          </a>
+        </Link>
         <div>
           <p>Projects</p>
         </div>
