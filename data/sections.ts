3b9e20afbb947de77459007b57db50ded23cd41c
Blankeos <carloantonioct@gmail.com>
2021-08-07 00:14:59 +0800

    Data-driven for section links in navs

---
+++ data/sections.ts
@@ -0,0 +1,19 @@
+const sections = [
+  {
+    name: "Projects",
+    href: "projects-section",
+  },
+  {
+    name: "Skills",
+    href: "skills-section",
+  },
+  {
+    name: "About Me",
+    href: "about-section",
+  },
+  {
+    name: "Contact",
+    href: "contact-section",
+  },
+];
+export default sections;
