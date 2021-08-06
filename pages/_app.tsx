f45bb7cb303b26f263e0428b9846668ccd57abc6
Blankeos <carloantonioct@gmail.com>
2021-08-07 00:15:46 +0800

    Refactored and cleaned up a large portion of the code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
+import "../src/styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
+import Layout from "../src/components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
