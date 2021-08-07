0430588f0b9604bc61930dd414bf33b743e382d1
Blankeos <carloantonioct@gmail.com>
2021-08-08 06:24:47 +0800

    Updated projects

--- data/projects.ts
+++ data/projects.ts
@@ -15,19 +15,19 @@ type Projects = Project[];
 const projects: Projects = [
   {
     title: "Cafe.ly",
-    shortDesc: "Full-Stack Social Product Review App",
+    shortDesc: "A Full-Stack Social Product Review App",
     slug: "cafely",
     imageURL:
       "https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/meta/preview_markdown.jpg",
     demoURL: "https://cafely.vercel.app/",
   },
   {
-    title: "Cafe.ly",
-    shortDesc: "Full-Stack Social Product Review App",
-    slug: "cafely",
+    title: "Cafe.ly Manual",
+    shortDesc: "An SSG documentation/blog site built with NextJS",
+    slug: "cafely-manual",
     imageURL:
-      "https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/meta/preview_markdown.jpg",
-    demoURL: "https://cafely.vercel.app/",
+      "https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/meta/manual_preview_markdown.jpg",
+    demoURL: "https://cafely-manual.vercel.app/",
   },
   {
     title: "Cafe.ly",
