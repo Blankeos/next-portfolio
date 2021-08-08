07ebfb0ef2c65ee352af0909b5aaff5e04a1adaf
Blankeos <carloantonioct@gmail.com>
2021-08-09 00:24:20 +0800

    Animation variants for easily animating delays on circles

--- src/components/FloatingCircle/index.tsx
+++ src/components/FloatingCircle/index.tsx
@@ -1,13 +1,56 @@
 import { CSSProperties } from "react";
-import { IconType } from "react-icons/lib";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
+export const getContainerVariants = (
+  staggerChildren: number = 0.2,
+  startDelay: number = 0
+): Variants => {
+  return {
+    hidden: {
+      y: 0,
+    },
+    visible: {
+      y: 0,
+      transition: {
+        when: "beforeChildren",
+        delay: startDelay,
+        staggerChildren: staggerChildren,
+      },
+    },
+  };
+};
+
+const nucleusVariants: Variants = {
+  hidden: { scale: 0 },
+  visible: {
+    scale: 1,
+    transition: {
+      duration: 0.8,
+      type: "spring",
+      when: "beforeChildren",
+      stiffness: 200,
+    },
+  },
+};
+
+const nucleusChildVariants: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.8,
+      ease: "circOut",
+    },
+  },
+};
 interface FloatingCircleProps {
   style: CSSProperties | undefined;
   orbitSize?: string;
   orbitClass?: string;
   nucleusClass?: string;
-  animationDelay?: number;
+  floatDelay?: number;
 }
 const FloatingCircle: React.FC<FloatingCircleProps> = ({
   style,
@@ -15,7 +58,7 @@ const FloatingCircle: React.FC<FloatingCircleProps> = ({
   orbitClass,
   nucleusClass,
   children,
-  animationDelay = 0,
+  floatDelay = 0,
 }) => {
   style = {
     ...style,
@@ -26,7 +69,7 @@ const FloatingCircle: React.FC<FloatingCircleProps> = ({
       style={style}
       initial={{ y: 0 }}
       animate={{ y: -25 }}
-      transition={{ yoyo: Infinity, duration: 2, delay: animationDelay }}
+      transition={{ yoyo: Infinity, duration: 2, delay: floatDelay }}
     >
       <Nucleus className={nucleusClass}>{children}</Nucleus>
       <Orbit orbitSize={orbitSize} className={orbitClass} />
@@ -61,20 +104,13 @@ const Nucleus: React.FC<NucleusProps> = ({
 }) => {
   return (
     <motion.div
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      transition={{ delay: 2, duration: 0.8, type: "spring" }}
+      variants={nucleusVariants}
       className="nucleus absolute rounded-full group cursor-pointer flex items-center justify-center z-10 h-20 w-20"
     >
       <span
         className={`absolute opacity-40 w-20 h-20 group-hover:w-28 group-hover:h-28 transition-all rounded-full ${className}`}
       />
-      <motion.span
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.4 }}
-        className="absolute"
-      >
+      <motion.span variants={nucleusChildVariants} className="absolute">
         {children}
       </motion.span>
     </motion.div>
