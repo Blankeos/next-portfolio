e7d5e382ec0c5aef34c882e5e5733dbab280c2db
Blankeos <carloantonioct@gmail.com>
2021-08-08 10:19:17 +0800

    Added better aspect ratio images

--- data/projects.ts
+++ data/projects.ts
@@ -1,10 +1,12 @@
 import { IconType } from "react-icons";
+import cafelyImg from "../src/assets/imgs/cafely_featured-project-img.jpg";
+import cafelyManualImg from "../src/assets/imgs/cafely-manual_featured-project-img.jpg";
 
 export type Project = {
   title: string;
   shortDesc: string;
   slug: string;
-  imageURL?: string;
+  imageURL?: string | StaticImageData;
   demoURL?: string;
 };
 
@@ -17,33 +19,29 @@ const projects: Projects = [
     title: "Cafe.ly",
     shortDesc: "A Full-Stack Social Product Review App",
     slug: "cafely",
-    imageURL:
-      "https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/meta/preview_markdown.jpg",
+    imageURL: cafelyImg,
     demoURL: "https://cafely.vercel.app/",
   },
   {
     title: "Cafe.ly Manual",
     shortDesc: "An SSG documentation/blog site built with NextJS",
     slug: "cafely-manual",
-    imageURL:
-      "https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/meta/manual_preview_markdown.jpg",
+    imageURL: cafelyManualImg,
     demoURL: "https://cafely-manual.vercel.app/",
   },
   {
     title: "Cafe.ly",
     shortDesc: "Full-Stack Social Product Review App",
     slug: "cafely",
-    imageURL:
-      "https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/meta/preview_markdown.jpg",
+    imageURL: cafelyImg,
     demoURL: "https://cafely.vercel.app/",
   },
   {
-    title: "Cafe.ly",
-    shortDesc: "Full-Stack Social Product Review App",
-    slug: "cafely",
-    imageURL:
-      "https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/meta/preview_markdown.jpg",
-    demoURL: "https://cafely.vercel.app/",
+    title: "Cafe.ly Manual",
+    shortDesc: "An SSG documentation/blog site built with NextJS",
+    slug: "cafely-manual",
+    imageURL: cafelyManualImg,
+    demoURL: "https://cafely-manual.vercel.app/",
   },
 ];
 
