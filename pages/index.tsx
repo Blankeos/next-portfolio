ccaba146689e63c41b841146fe30ea34387ffc89
Blankeos <carloantonioct@gmail.com>
2021-08-10 05:53:44 +0800

    Edited homepage root

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,7 +69,7 @@ const Home: React.FC = () => {
 
 const BreakPointHelper = () => {
   return (
-    <div className="bg-green-400 sm:bg-blue-600 md:bg-red-600 lg:bg-yellow-300 xl:bg-indigo-400 2xl:bg-pink-500 fixed top-0 left-0 text-xs flex space-x-4 p-2 z-50">
+    <div className="bg-green-400 sm:bg-blue-600 md:bg-red-600 lg:bg-yellow-300 xl:bg-indigo-400 2xl:bg-pink-500 fixed top-0 left-0 text-xs flex space-x-4 p-2 z-50 w-full">
       <div>xs: green</div>
       <div>sm: blue</div>
       <div>md: red</div>
