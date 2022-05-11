e79e643d41bd3aa9ce6bb10f2357931e02c22b5e
Blankeos <carloantonioct@gmail.com>
2022-05-12 04:44:26 +0800

    Fixed hot reload bug by resetting dependencies (framer-motion might be the culprit)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["static.wikia.nocookie.net", "raw.githubusercontent.com"],
   },
 };
+
+module.exports = nextConfig;
