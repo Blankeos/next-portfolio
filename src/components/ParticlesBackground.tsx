b3d499736cdfc92878511043880a0e02b34d4cbb
Blankeos <carloantonioct@gmail.com>
2022-11-24 22:56:15 +0800

    Fixed bug with tsparticles-engine

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -1,12 +1,11 @@
-// tsparticles
 import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import particlesConfig from "../particles/particles.json";
-// import type { Engine, Container, ISourceOptions } from "tsparticles-engine";
+import type { Engine, Container, ISourceOptions } from "tsparticles-engine";
 
 const ParticlesBackground: React.FC = () => {
-  const particlesInit = useCallback(async (engine: any) => {
+  const particlesInit = useCallback(async (engine: Engine) => {
     console.log(engine);
 
     // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
@@ -30,11 +29,3 @@ const ParticlesBackground: React.FC = () => {
 };
 
 export default ParticlesBackground;
-
-// Source for this code: yarn create react-app --template particles-typescript
-//
-// Implementation from the template project:
-// const particlesInit = useCallback(async (engine: Engine) => {
-//     await loadFull(engine);
-// }, []);
-// <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
