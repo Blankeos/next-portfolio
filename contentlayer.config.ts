ba62f157fc160551e6041e310d45edc63f43d08b
Blankeos <carloantonioct@gmail.com>
2023-11-12 07:33:37 +0800

    feat: reading time + working projects content.

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,10 +2,13 @@
 
 // contentlayer.config.ts
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import path from 'path'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
+import fs from 'node:fs'
+import readingTime from 'reading-time'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -22,13 +25,26 @@ export const Post = defineDocumentType(() => ({
     },
   },
   computedFields: {
+    readTimeStats: {
+      type: 'nested',
+      resolve: (post) => {
+        const content = fs.readFileSync(
+          path.join('content', post._raw.sourceFilePath),
+          'utf-8'
+        )
+
+        const body = content.split('---').at(-1)?.trim() ?? ''
+
+        return readingTime(body)
+      },
+    },
     slug: {
       type: 'string',
       resolve: (post) => `${post._raw.flattenedPath.split('/').at(1)}`,
     },
     url: {
       type: 'string',
-      resolve: (post) => `/content/posts/${post._raw.flattenedPath}`,
+      resolve: (post) => `/content/${post._raw.flattenedPath}`,
     },
   },
 }))
@@ -42,11 +58,10 @@ export const Project = defineDocumentType(() => ({
     description: { type: 'string', required: true },
     date: { type: 'date', required: true },
     featuredImage: { type: 'string', required: true },
-    isFeatured: {
-      type: 'boolean',
-      description:
-        "True to include on the 'featured projects' in the home page.",
-    },
+    featureOrder: { type: 'number' },
+    demoURL: { type: 'string' },
+    featuredYoutubeURL: { type: 'string' },
+    tags: { type: 'list', of: { type: 'string' } },
   },
   computedFields: {
     slug: {
@@ -55,14 +70,14 @@ export const Project = defineDocumentType(() => ({
     },
     url: {
       type: 'string',
-      resolve: (project) => `/content/projects/${project._raw.flattenedPath}`,
+      resolve: (project) => `/content/${project._raw.flattenedPath}`,
     },
   },
 }))
 
 export default makeSource({
   contentDirPath: 'content',
-  documentTypes: [Post],
+  documentTypes: [Post, Project],
 
   // Plugins
   mdx: {
@@ -73,17 +88,17 @@ export default makeSource({
         rehypePrettyCode,
         {
           theme: 'github-dark',
-          onVisitLine(node) {
+          onVisitLine(node: any) {
             // Prevent lines from collapsing in `display: grid` mode, and allow empty
             // lines to be copy/pasted
             if (node.children.length === 0) {
               node.children = [{ type: 'text', value: ' ' }]
             }
           },
-          onVisitHighlightedLine(node) {
+          onVisitHighlightedLine(node: any) {
             node.properties.className = [`line--highlighted`]
           },
-          onVisitHighlightedWord(node) {
+          onVisitHighlightedWord(node: any) {
             node.properties.className = [`word--highlighted`]
           },
         },
