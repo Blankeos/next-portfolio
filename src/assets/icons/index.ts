83ecbeb6674e07cc25a8704f3f81640e533e2920
Blankeos <carloantonioct@gmail.com>
2025-05-19 02:01:54 +0800

    feat: added next-way to import icons typesafely.

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -8,3 +8,19 @@
 export { RiArrowUpLine as IconArrowUp } from 'react-icons/ri';
 export { default as IconLoadingHorizontal } from './loading-horizontal';
 export { default as IconLoadingLoop } from './loading-loop';
+
+export { default as IconBriefcase } from './iconamoon_briefcase-duotone.svg';
+export { default as IconClose } from './iconamoon_close-bold.svg';
+export { default as IconHome } from './iconamoon_home-duotone.svg';
+export { default as IconPen } from './iconamoon_pen-duotone.svg';
+export { default as IconSearch } from './iconamoon_search-duotone.svg';
+export { default as IconSmilingFace } from './iconamoon_smiling-face-duotone.svg';
+export { default as IconChevronLeft } from './material-symbols_chevron-left.svg';
+export { default as IconPaint } from './ph_paint-bucket-duotone.svg';
+export { default as IconPalette } from './ph_palette-duotone.svg';
+
+export { default as IconGithubAlt } from './fa6-brands_github-alt.svg';
+export { default as IconInstagram } from './fa6-brands_instagram.svg';
+export { default as IconLinkedIn } from './fa6-brands_linkedin-in.svg';
+export { default as IconCommandKey } from './material-symbols_keyboard-command-key.svg';
+export { default as IconCamera } from './pepicons-pop_photo-camera.svg';
